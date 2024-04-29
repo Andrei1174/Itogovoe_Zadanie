cce1e8fd6ab15982dab31d5c79eee0bb70d9cd5f
Andrei1174 <aremin1174@gmail.com>
2024-04-30 08:18:44 +0900

    создали масссив строк

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,8 @@
 ﻿
+string[] GreateArrayStringConsole (string ConsolStr)
+{
+    string[] arrayStr;
+    string[] _ = new string[ConsolStr.Length];
+    arrayStr = ConsolStr.Split(",");
+    return arrayStr;
+}
