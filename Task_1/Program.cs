14b84a63c4c70695fd08a0f5f190b028601051bf
Andrei1174 <aremin1174@gmail.com>
2024-04-30 09:12:45 +0900

    создали клиентский ввод и вызов функций

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,3 +6,14 @@ string[] GreateArrayStringConsole (string ConsolStr)
     arrayStr = ConsolStr.Split(",");
     return arrayStr;
 }
+
+Console.Write("Введите набор символов через знак (,) - ");
+string StringStr = Console.ReadLine()!;
+string[] ArrayStr = GreateArrayStringConsole(StringStr);
+
+Console.Write("Введённый массив строк -> ");
+Print(ArrayStr);
+Console.WriteLine();
+
+Console.Write($"Введённые символы длинной меньше либо равны 3 -> ");
+Print(GreateArrGivenSize(ArrayStr));
